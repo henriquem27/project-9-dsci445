4aa2564c8cd3f5212e1ee7dcc90a2e4d359b84c2
Henrique Rio <henriquerio27@gmail.com>
2021-11-27 23:40:09 +0000

    fix

--- pca.R
+++ pca.R
@@ -1,13 +1,22 @@
 ###
 
 
-
-train <- read.csv("train.csv")
-
+train <- read.csv("train.csv")head(train)
 
 
+train$ChestPainType <- as.factor(train$ChestPainType)
+train$Sex <- as.factor(train$Sex)
+train$FastingBS <- as.factor(train$FastingBS)
+train$RestingECG <- as.factor(train$RestingECG)
+train$ExerciseAngina <- as.factor(train$ExerciseAngina)
+train$ST_Slope <- as.factor(train$ST_Slope)
+train$HeartDisease <- as.factor(train$HeartDisease)
 
-pca <- pca <- prcomp(train[,c("RestingBP","Cholesterol","HeartDisease","Oldpeak","MaxHR")], center = TRUE, scale = TRUE)
 
+
+pca <- prcomp(train[,c("RestingBP","Cholesterol","HeartDisease","Oldpeak","MaxHR")], center = TRUE, scale = TRUE)
+
+# ran into some problems, turn categorical variables into binary variable?
+pca2 <- prcomp(train,center = TRUE,scale = TRUE)
 
 biplot(pca)
