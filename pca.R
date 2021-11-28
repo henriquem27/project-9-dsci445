2b393e18e4cd4b7ecdfd6efbfa4cbc2998c8deac
Henrique Rio <henriquerio27@gmail.com>
2021-11-28 00:07:42 +0000

    Trying to get PCA working

--- pca.R
+++ pca.R
@@ -1,22 +1,26 @@
 ###
 
+train <- read.csv("train.csv")
 
-train <- read.csv("train.csv")head(train)
 
 
-train$ChestPainType <- as.factor(train$ChestPainType)
-train$Sex <- as.factor(train$Sex)
-train$FastingBS <- as.factor(train$FastingBS)
-train$RestingECG <- as.factor(train$RestingECG)
-train$ExerciseAngina <- as.factor(train$ExerciseAngina)
-train$ST_Slope <- as.factor(train$ST_Slope)
-train$HeartDisease <- as.factor(train$HeartDisease)
+str(train)
+
+# try to make 
 
 
 
 pca <- prcomp(train[,c("RestingBP","Cholesterol","HeartDisease","Oldpeak","MaxHR")], center = TRUE, scale = TRUE)
 
-# ran into some problems, turn categorical variables into binary variable?
+# PCA does not work for categorical variabels.
 pca2 <- prcomp(train,center = TRUE,scale = TRUE)
 
-biplot(pca)
+
+# try to make dummy variables
+train$CPTASY <- ifelse(train$ChestPainType == 'ASY', 1, 0)
+
+str(train)
+
+pca2 <- prcomp(train[,c("RestingBP","Cholesterol","HeartDisease","Oldpeak","MaxHR","CPTASY")],center = TRUE,scale = TRUE)
+
+biplot(pca2)
